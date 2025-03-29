cfd361a3de6cc386838fd2a0ce91e8646f304066
Mowmita Yasmin <meite@academin.rrc.ca>
2025-03-29 09:50:36 -0500

    Implemented branch and loan model

---
+++ src/api/v1/models/loan.ts
@@ -0,0 +1,42 @@
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     Loan:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: number
+ *         name: 
+ *           type: string
+ *         price:
+ *           type: number
+ *         description:
+ *           type: string
+ *         is_reviewed:
+ *           type: boolean
+ *         is_approved:
+ *           type: boolean
+ *         created_at:
+ *           type: string
+ *           format: date-time
+ *         update_at:
+ *           type: string
+ *           format: date-time
+ *       required:
+ *         - id
+ *         - name
+ *         - description
+ *         - is_reviewed
+ *         - is_approved
+ */
+export type Loan = {
+    id: number,
+    name: string;
+    description: string;
+    price?: number;
+    created_at: Date;
+    update_at: Date;
+    is_reviewed: Boolean;
+    is_approved: Boolean
+};
