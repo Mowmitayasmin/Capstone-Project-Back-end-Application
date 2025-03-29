56e590d4de84334b52a6214e01241e6bc4f646f0
Mowmita Yasmin <meite@academin.rrc.ca>
2025-03-29 10:23:41 -0500

    Implement user, loan & branch services for CRUD operation

---
+++ src/api/v1/services/loan.ts
@@ -0,0 +1,37 @@
+import { Loan } from "../models/loan";
+import {
+    getItems,
+    createItems,
+    updateItems,
+    deleteItems
+} from "../repositories/firestore";
+
+const COLLECTION = "loans";
+
+export const getAllLoans = async (): Promise<Loan[]> => {
+    const snapshot: FirebaseFirestore.QuerySnapshot = await getItems(
+        COLLECTION
+    );
+
+    return snapshot.docs.map((doc) => {
+        const data: FirebaseFirestore.DocumentData = doc.data();
+        return { id: doc.id, ...data } as unknown as Loan;
+    });
+};
+
+export const createLoan = async (item: Partial<Loan>): Promise<Loan> => {
+    const id: string = await createItems(COLLECTION, item);
+    return { id, ...item } as Loan;
+};
+
+export const updateLoan = async (
+    id: string,
+    item: Partial<Loan>
+): Promise<Loan> => {
+    await updateItems(COLLECTION, id, item);
+    return { id, ...item } as Loan;
+};
+
+export const deleteLoan = async (id: string): Promise<void> => {
+    await deleteItems(COLLECTION, id);
+};
