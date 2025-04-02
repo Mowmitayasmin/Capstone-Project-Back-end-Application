feba39df72542736fe7443520d7f405601dcd53b
Mowmita Yasmin <meite@academin.rrc.ca>
2025-04-02 09:27:39 -0500

    Implement all routes in app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,12 @@ import helmet from "helmet";
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import cors from 'cors';
+import swaggerConfig from '../config/swagger';
+import customErrorHandler from './api/v1/middleware/customErrorHandler';
+import loanRouter from './api/v1/routes/loan';
+import userRoutes from './api/v1/routes/user';
+import adminRoutes from './api/v1/routes/admin';
+import branchRoutes from './api/v1/routes/branch';
 dotenv.config();
   
 const app : Express = express();
@@ -11,9 +17,18 @@ app.use(morgan('combined'))
 app.use(express.json());
 app.use(cors())
 
+app.use('/api/v1/loan', loanRouter)
+app.use('/api/v1/user', userRoutes)
+app.use('/api/v1/admin', adminRoutes)
+app.use('/api/v1/branch', branchRoutes)
+
+swaggerConfig(app)
+
 app.get("/", (req, res) => {
     res.send('You are landed an empty ocean')
 })
+
+app.use(customErrorHandler)
 
 export default app
 
